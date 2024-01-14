dacdd294ca2c20fcb00c91ac324f85f98f6a412d
Zachareee <zacharytanhx@yahoo.com>
2024-01-14 21:59:38 +0800

    Added test cases for Q3-6

--- tests/week1/Q6Test.java
+++ tests/week1/Q6Test.java
@@ -1,11 +1,25 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class Q6Test {
     @Test
-    public void testEmpty() {
-        boolean matchHiAndBye = Q6.matchStrings("Hi", "Bye");
-        assertEquals(matchHiAndBye, true);
+    public void testNonMatching() {
+        assertFalse(Q6.matchStrings("Hija", "Bye"));
+        assertFalse(Q6.matchStrings("Longerstring", " "));
+    }
+
+    @Test
+    public void testPartialMatch() {
+        assertFalse(Q6.matchStrings("Hi there", "ieea"));
+        assertFalse(Q6.matchStrings("Hi there", "i e "));
+        assertFalse(Q6.matchStrings("donkey string", " nf"));
+    }
+
+    @Test
+    public void testFullMatch() {
+        assertTrue(Q6.matchStrings("a really long string", "a ayl stg"));
+        assertTrue(Q6.matchStrings("eeeeeeeeeee", "eeeee"));
+        assertTrue(Q6.matchStrings("aeiouaeiou", "aiueio"));
     }
 }
